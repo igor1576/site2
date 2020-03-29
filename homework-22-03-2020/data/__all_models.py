b6e001df9e7385e354eab3227109f92e35494d70
Admin <igor@gymn1576.ru>
2020-03-29 18:21:47 +0300

    Site

--- homework-22-03-2020/data/__all_models.py
+++
@@ -1,3 +0,0 @@
-from . import users
-from . import homework
-from . import faq
--- homework-22-03-2020/data/faq_answer_form.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import SubmitField, StringField, PasswordField, BooleanField
-from wtforms.fields.html5 import EmailField
-from wtforms.validators import DataRequired
-
-
-class FaqAnswerForm(FlaskForm):
-    answer = StringField('Напишите свой ответ', validators=[DataRequired()])
-    submit = SubmitField('Отправить')
